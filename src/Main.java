08db432d2b437061d5f7ae7bee50ff93a115765f
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-05-12 16:52:14 +0700

    ФП3. Итерация 2

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,93 @@
 import logic.Manager;
+import task.Epic;
+import task.Subtask;
+import task.Task;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Поехали!");
         Manager manager = new Manager();
-        manager.createTask("Задача 1.", "Выполнить задачу 1.", "NEW");
-        manager.createTask("Задача 2.", "Выполнить задачу 2.", "NEW");
-        manager.createEpic("Эпик 1.", "Эпик 1 выполняется.");
-        manager.createEpic("Эпик 2.", "Выполнить эпик 2.");
-        manager.createSubTask("Подзадача 1.1.", "Выполняется подзадача 1.1.",
-                "IN_PROGRESS", 3);
-        manager.createSubTask("Подзадача 1.2.", "Выполнить подзадачу 1.2.",
-                "NEW", 3);
-        manager.createSubTask("Подзадача 2.1.", "Выполнить подзадачу 2.1.",
-                "NEW", 4);
+
+        Task task1 = new Task();
+        task1.setNameTask("Задача 1.");
+        task1.setDescriptionTask("Выполнить задачу 1.");
+        task1.setStatusTask("NEW");
+        manager.createTask(task1);
+        Task task2 = new Task();
+        task2.setNameTask("Задача 2.");
+        task2.setDescriptionTask("Выполняется задача 2.");
+        task2.setStatusTask("IN_PROGRESS");
+        manager.createTask(task2);
+
+        Epic epic1 = new Epic();
+        epic1.setNameTask("Эпик 1.");
+        epic1.setDescriptionTask("Выполнить эпик 1.");
+        manager.createEpic(epic1);
+
+        Subtask subtask1 = new Subtask();
+        subtask1.setNameTask("Подзадача 1.1.");
+        subtask1.setDescriptionTask("Выполнить подзадачу 1.1.");
+        subtask1.setStatusTask("NEW");
+        subtask1.setIdEpic(3);
+        manager.createSubTask(subtask1);
+        Subtask subtask2 = new Subtask();
+        subtask2.setNameTask("Подзадача 1.2.");
+        subtask2.setDescriptionTask("Выполнить подзадачу 1.2.");
+        subtask2.setStatusTask("NEW");
+        subtask2.setIdEpic(3);
+        manager.createSubTask(subtask2);
+
+        Epic epic2 = new Epic();
+        epic2.setNameTask("Эпик 2.");
+        epic2.setDescriptionTask("Выполняется эпик 2.");
+        manager.createEpic(epic2);
+
+        Subtask subtask3 = new Subtask();
+        subtask3.setNameTask("Подзадача 2.1.");
+        subtask3.setDescriptionTask("Выполняется подзадача 2.1.");
+        subtask3.setStatusTask("IN_PROGRESS");
+        subtask3.setIdEpic(6);
+        manager.createSubTask(subtask3);
+
+        System.out.println("Создаем задачи:");
         System.out.println(manager.getAllTask());
+        System.out.println("Создаем эпики:");
         System.out.println(manager.getAllEpic());
+        System.out.println("Создаем подзадачи:");
         System.out.println(manager.getAllSubTask());
-        manager.updateTask("Задача 1.", "Задача 1 выполнена.",
-                "DONE", 1);
-        manager.updateTask("Задача 2.", "Задача 2 выполняется.",
-                "IN_PROGRESS", 2);
-        manager.updateEpic("Эпик 1.",
-                "Эпик 1 выполнен, так как все его подзадачи выполнены.", 3);
-        manager.updateEpic("Эпик 2.",
-                "Эпик 2 выполняется, так как его подзадача выполняется.", 4);
-        manager.updateSubTask("Подзадача 1.1.", "Подзадача 1.1 выполнена.",
-                "DONE", 5);
-        manager.updateSubTask("Подзадача 1.2.", "Подзадача 1.2 выполнена.",
-                "DONE", 6);
-        manager.updateSubTask("Подзадача 2.1.", "Подзадача 2.1 выполняется.",
-                "IN_PROGRESS", 7);
+
+        task1.setDescriptionTask("Выполняется задача 1.");
+        task1.setStatusTask("IN_PROGRESS");
+        manager.updateTask(task1);
+
+
+        task2.setDescriptionTask("Задача 2 выполнена.");
+        task2.setStatusTask("DONE");
+        manager.updateTask(task2);
+
+        subtask1.setDescriptionTask("Выполняется подзадача 1.1.");
+        subtask1.setStatusTask("IN_PROGRESS");
+        manager.updateSubTask(subtask1);
+
+        epic1.setDescriptionTask("Выполняется эпик 1.");
+        manager.updateEpic(epic1);
+
+        System.out.println("Обновили задачи:");
         System.out.println(manager.getAllTask());
+        System.out.println("Обновили эпики:");
         System.out.println(manager.getAllEpic());
+        System.out.println("Обновили подзадачи:");
         System.out.println(manager.getAllSubTask());
-        manager.deleteTaskId(2);
+
+        manager.deleteTaskId(1);
         manager.deleteEpicId(3);
         manager.deleteSubTaskId(7);
+
+        System.out.println("Удалили задачу:");
         System.out.println(manager.getAllTask());
+        System.out.println("Удалили эпики:");
         System.out.println(manager.getAllEpic());
+        System.out.println("Удалили подзадачи:");
         System.out.println(manager.getAllSubTask());
     }
 }
