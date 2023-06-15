395570ca655e3c6ceadb012e4c7d4576bcca9e15
Dmitrii_Z <zolotuhindimitrii@yandex.ru>
2023-06-15 23:36:54 +0700

    Обновлено для ФП5

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         TaskManager taskManager = Managers.getDefault();
 
-        for (int i = 1; i <= 15; i++) {
+        for (int i = 1; i <= 2; i++) {
             Task task = new Task();
             task.setNameTask("Задача  " + i + ".");
             task.setDescriptionTask("Описание задачи " + i + ".");
@@ -17,19 +17,19 @@ public class Main {
             taskManager.createTask(task);
         }
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 3; i <= 4; i++) {
             Epic epic = new Epic();
             epic.setNameTask("Эпик " + i + ".");
             epic.setDescriptionTask("Описание эпика " + i + ".");
             taskManager.createEpic(epic);
         }
 
-        for (int i = 1; i <= 15; i++) {
+        for (int i = 6; i <= 8; i++) {
             Subtask subtask = new Subtask();
             subtask.setNameTask("Подзадача " + i);
             subtask.setDescriptionTask("Описание подзадачи " + i);
             subtask.setStatusTask(StatusTask.NEW);
-            subtask.setIdEpic(15 + i);
+            subtask.setIdEpic(3);
             taskManager.createSubTask(subtask);
         }
 
@@ -37,41 +37,38 @@ public class Main {
         System.out.println("Кол-во созданных эпиков: " + taskManager.getAllEpic().size());
         System.out.println("Кол-во созданных подзадач: " + taskManager.getAllSubTask().size());
 
-        for (int i = 13; i > 8; i--) {
-            Task task = taskManager.getTask(i);
-        }
+        Task task = taskManager.getTask(2);
+        Task task1 = taskManager.getTask(1);
 
-        System.out.println("\nВызвали задачи.");
-        System.out.println("Кол-во подзадач в истории: " + taskManager.getHistory().size());
-        System.out.println("Состав истории задач:");
-        for (Task task : taskManager.getHistory()) {
-            System.out.println(task);
-        }
+        System.out.println(taskManager.getHistory());
 
-        Subtask subtask = taskManager.getSubTaskId(29);
+        Task task2 = taskManager.getTask(1);
+        Task task3 = taskManager.getTask(2);
 
-        for (int i = 20; i < 23; i++) {
-            Epic epic = taskManager.getEpicId(i);
-        }
+        System.out.println(taskManager.getHistory());
 
-        System.out.println("\nВызвали задачу и эпики.");
-        System.out.println("Кол-во подзадач в истории: " + taskManager.getHistory().size());
-        System.out.println("Состав истории задач:");
-        for (Task task : taskManager.getHistory()) {
-            System.out.println(task);
-        }
+        Epic epic = taskManager.getEpicId(4);
+        Task task4 = taskManager.getTask(1);
 
-        for (int i = 35; i > 33; i--) {
-            Subtask subtask1 = taskManager.getSubTaskId(i);
-        }
+        System.out.println(taskManager.getHistory());
 
-        Task task2 = taskManager.getTask(2);
+        Epic epic1 = taskManager.getEpicId(3);
+        Task task5 = taskManager.getTask(2);
 
-        System.out.println("\nВызвали задачу и подзадачи.");
-        System.out.println("Кол-во подзадач в истории: " + taskManager.getHistory().size());
-        System.out.println("Состав истории задач:");
-        for (Task task : taskManager.getHistory()) {
-            System.out.println(task);
-        }
+        System.out.println(taskManager.getHistory());
+
+        Subtask subtask = taskManager.getSubTaskId(5);
+        Subtask subtask1 = taskManager.getSubTaskId(6);
+        Subtask subtask2 = taskManager.getSubTaskId(7);
+
+        System.out.println(taskManager.getHistory());
+
+        taskManager.deleteTaskId(1);
+
+        System.out.println(taskManager.getHistory());
+
+        taskManager.deleteEpicId(3);
+
+        System.out.println(taskManager.getHistory());
     }
 }
